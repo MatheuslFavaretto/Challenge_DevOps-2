afb0a3862a9e41d2d35cb05992c20df05b563073
MatheuslFavaretto <matheusfavaretto.ef@gmail.com>
2023-05-27 11:26:03 -0300

    CI/CD

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-pkill java
+sudo yum update
+sudo yum install maven git -y
 sudo rm -rf api
 git clone https://github.com/rcaneppele/2771-spring-boot.git api && cd api
 mvn clean package -DskipTests
-nohup java -Dspring.profiles.active=prod -DDATASOURCE_URL=jdbc:mysql://dev-db.c69d0zwlxyuz.us-east-1.rds.amazonaws.com/vollmed_api -DDATASOURCE_USERNAME=sysadmin -DDATASOURCE_PASSWORD=123456789 -jar target/api-0.0.1-SNAPSHOT.jar > out.txt 2>&1 &

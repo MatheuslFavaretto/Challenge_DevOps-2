63f1b7c89a1b79d77118a60d3c7710c95e0013df
MatheuslFavaretto <matheusfavaretto.ef@gmail.com>
2023-05-27 18:06:32 -0300

    CI/CD

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ sudo yum update
 sudo yum install maven git -y
 pkill java
 sudo rm -rf api
-git clone https://github.com/MatheuslFavaretto/-Challenge_DevOps-2.git api && cd api
+git clone https://github.com/MatheuslFavaretto/Challenge_DevOps-2.git api && cd api
 mvn clean package -DskipTests
 nohup java -Dspring.profiles.active=prod -DDATASOURCE_URL=jdbc:mysql://dev-db.c69d0zwlxyuz.us-east-1.rds.amazonaws.com:3306/vollmed_api -DDATASOURCE_USERNAME=sysadmin -DDATASOURCE_PASSWORD=123456 -jar target/api-0.0.1-SNAPSHOT.jar > out.txt 2>&1 &
 

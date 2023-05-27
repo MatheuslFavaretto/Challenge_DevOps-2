7d5b4604f3ca202c45d5fd1da865cce794f23832
MatheuslFavaretto <matheusfavaretto.ef@gmail.com>
2023-05-27 17:17:59 -0300

    CI|CD

--- infra/aws/outputs.tf
+++ infra/aws/outputs.tf
@@ -5,8 +5,3 @@ output "vm_ip" {
 output "rds_ip" {
   value = ["DB Endpoint: ", aws_db_instance.api_db.endpoint]
 }
-
-output "rds_test_ip" {
-  value = ["DB_test Endpoint: ", aws_db_instance.api_db_test.endpoint]
-
-}
--- src/main/java/med/voll/api/controller/HelloController.java
+++ src/main/java/med/voll/api/controller/HelloController.java
@@ -1,4 +1,3 @@
-package med.voll.api.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
